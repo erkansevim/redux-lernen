28626cc20621e32c6d341cb0396e3a07075b8625
erkansevim <erkansevim.essgmail.com>
2024-08-26 15:24:18 +0200

    reducers combined

--- src/redux/reducers/counterReducer.js
+++ src/redux/reducers/counterReducer.js
@@ -1,6 +1,6 @@
 import * as actionTypes from "../actions/actionTypes";
 
-const conterReducer = (state = 0, action) => {
+const counterReducer = (state = 0, action) => {
   let newState;
   switch (action.type) {
     case actionTypes.INCREASE_COUNTER:
@@ -14,3 +14,5 @@ const conterReducer = (state = 0, action) => {
       return state;
   }
 };
+
+export default counterReducer
